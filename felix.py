3ebac0b290d7108c873842b4542d59cbfa991352
foolishfish21 <129061961+foolishfish21@users.noreply.github.com>
2023-03-27 13:25:53 +0200

    Add files via upload

---
+++ felix.py
@@ -0,0 +1,5 @@
+import random
+chosen = random.randint(0,8)
+texts = ["remind me when I asked", "zizouuuuuuu", "messi simp", "where the hell is my pencil case", "what colour is your father", "1v1 me in real life, OH WAIT! YOU DONT HAVE A REAL LIFE!", "balls", "I like men :)"]
+
+print(texts[(chosen)])
